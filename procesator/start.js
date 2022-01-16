de53e0f7b058f7b2cfa7cbce2fa3b38feada2dad
Cristi Condruz <cristi.condruz98@gmail.com>
2022-01-16 21:52:35 +0200

    Fixed procesator connection

--- procesator/start.js
+++ procesator/start.js
@@ -6,50 +6,61 @@ const { ExecuteQuery } = require("./database");
 
 const QUEUE = "appointments";
 
-const init = async () => {
-  while (true) {
-    try {
-      const connection = await amqp.connect(
-        process.env.NODE_ENV === "development"
-          ? "amqp://localhost"
-          : process.env.AMQPURL
-      );
-      const channel = await connection.createChannel();
-      channel.assertQueue(QUEUE, {
-        durable: false,
-      });
+const reconnectToBroker = () => {
+  console.error("[AMQP] reconnecting");
+  connectToBroker();
+}
 
-      console.log(
-        " [*] Waiting for messages in %s. To exit press CTRL+C",
-        QUEUE
-      );
-
-      channel.consume(QUEUE, async (msg) => {
-        console.log(" [x] Received %s", msg.content.toString());
-
-        if (msg !== null) {
-          const jsonPayload = JSON.parse(msg.content);
-          try {
-            await ExecuteQuery(
-              "INSERT INTO appoinments (full_name, time_interval, service_id) VALUES ($1, $2, $3)",
-              [
-                jsonPayload.full_name,
-                jsonPayload.time_interval,
-                jsonPayload.service_id,
-              ]
-            );
-          } catch (err) {
-            console.error(err);
-          }
+const connectToBroker = () => {
+  console.log(`${process.env.AMQPURL}`)
+  amqp.connect(`${process.env.AMQPURL}`).then(
+    async function(conn) {
+      conn.on("error", function(err) {
+        if (err.message !== "Connection closing") {
+          console.error("[AMQP] connection error", err.message);
         }
-
-        channel.ack(msg);
+      });
+      
+      conn.on("close", function() {
+        console.error("Connection closed. Reconnecting in 15s!");
+        return setTimeout(reconnectToBroker, 15000);
       });
 
-      break;
-    } catch (error) {
-      await new Promise((resolve) => setTimeout(resolve, 1000));
+      const ch = await conn.createChannel();
+      const eventQueue = await ch.assertQueue(QUEUE, { durable: true });
+      
+      await ch.consume(eventQueue.queue, handleEvent, { noAck: false });
+
+      console.log("Broker connection and subscriptions are established!");
+    }).catch(ex => {
+      console.error("Couldn't connect to message queue!");
+      console.error(ex);
+      console.error("Reconnecting in 5s!");
+
+      return setTimeout(reconnectToBroker, 5000);
+    });
+};
+
+const handleEvent = async (msg) => {
+  console.log(" [x] Received %s", msg.content.toString());
+
+  if (msg !== null) {
+    const jsonPayload = JSON.parse(msg.content);
+    try {
+      await ExecuteQuery(
+        "INSERT INTO appoinments (full_name, time_interval, service_id) VALUES ($1, $2, $3)",
+        [
+          jsonPayload.full_name,
+          jsonPayload.time_interval,
+          jsonPayload.service_id,
+        ]
+      );
+    } catch (err) {
+      console.error(err);
     }
   }
-};
-init();
+
+  channel.ack(msg);
+}
+
+connectToBroker();
